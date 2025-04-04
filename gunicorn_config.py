cc9ce4c83f304599e5ccdddffcb5b7b53dd9bef5
Alonso Valdes <alonsoo8.5@gmail.com>
2025-04-04 19:18:06 +0200

    smaller specs for prod

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 # Configuración para mejor manejo de recursos
-workers = 3  # Número de workers
-threads = 2  # Threads por worker
+workers = 2  # Número de workers
+threads = 1  # Threads por worker
 worker_class = 'gthread'  # Usar threads
 worker_connections = 1000
-timeout = 30
-keepalive = 2 
+timeout = 300
+keepalive = 2
